b012fd5e3adf8fa101d2ff01983cb49e2443ca8e
Kertich <kipsangkertich@gmail.com>
2023-02-25 11:30:32 +0300

    generate components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductspageComponent } from './productspage/productspage.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { AdminpageComponent } from './adminpage/adminpage.component';
+import { SingleproductComponent } from './singleproduct/singleproduct.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ProductspageComponent,
+    AuthenticationComponent,
+    AdminpageComponent,
+    SingleproductComponent,
+    CartComponent,
+    CheckoutComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
